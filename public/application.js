286ede4c05eb5621a85839e37e2cd79543eb3e46
lbracken <levi.bracken@gmail.com>
2015-11-28 08:44:43 -0600

    Adding material design lite and header/navigation drawer

--- public/application.js
+++ public/application.js
@@ -8,6 +8,16 @@ angular.module(ApplicationConfiguration.applicationModuleName).config(['$locatio
 	}
 ]);
 
+// Run some logic each item Angular refreshes the view
+angular.module(ApplicationConfiguration.applicationModuleName).run(function ($rootScope, $timeout) {
+	$rootScope.$on('$viewContentLoaded', ()=> {
+		$timeout(() => {
+			// Register dynamically created DOM elements with Material Design Lite
+			componentHandler.upgradeAllRegistered();
+		})
+	})
+});
+
 angular.element(document).ready(function(){
 	// Init the app
 	angular.bootstrap(document, [ApplicationConfiguration.applicationModuleName]);
